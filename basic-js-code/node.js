30dfd318258eaa638744d76c1d9dab72d7cd36e7
KawulJan <aytoldi.uii@gmail.com>
2022-11-18 19:21:37 +0900

    javascript projects for study

---
+++ basic-js-code/node.js
@@ -0,0 +1,13 @@
+// getElementById -> get element by ID 
+// querySelector -> get CSS's select element , get first element
+// querySelectorAll -> get CSS's Select element. nodeList by as like row 
+// documentElement -> get html tag
+//head   -> get head tag
+
+const list = document.querySelector('.list')
+console.log(list)
+
+//console.log(document.querySelector(".list li a"))
+//for (let i = 0; i < document.querySelectorAll(".list li a ").length; i++){
+  //  document.querySelectorAll(".list li a")[i].textContent = "test" +i
+//}
